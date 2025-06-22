0a5cb1839e38619e1a682127a1c00c9744deb003
Daniel <danielmayombo47@gmail.com>
2025-06-22 22:00:47 +0200

    dd

--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -113,7 +113,7 @@ router.get('/me', verifyTeacherToken, async (req, res) => {
       totalCourses: teacher.courses.length,
     };
 
-    res.status(200).json(teacherInfo);
+    res.status(200).json(teacher);
   } catch (err) {
     res.status(500).json({ message: 'Erreur lors de la récupération des informations du professeur.', error: err.message });
   }
